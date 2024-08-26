c85233bc282e7c8214cd07ec47f13ed59f59fb16
harshavardh8n <harshavardhangaikwad39@gmail.com>
2024-08-27 01:37:18 +0530

    testing

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
 const {router} = require("./router/response")
 const {crouter} = require("./router/compareresp")
 const {crrouter} = require("./router/createresume")
-const {uploadImage}  = require("./services/cloudinaryupload")
 // const {grouter} = require("./router/geminianalyse")
 
 
  // Ensure you have 'path' module imported for directory path
 
 const app = express();
-
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE,PATCH,HEAD',
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
@@ -27,9 +18,7 @@ app.use(router)
 app.use(crouter)
 app.use(crrouter)
 
-app.use("/uploadeer",()=>{
-    uploadImage();
-})
+
 app.use("/",(req,res)=>{
     res.send("works")
 })
